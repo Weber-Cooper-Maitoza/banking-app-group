f7adc795184220448b5e7eb7f2b87b7bbb64ead3
Coleton Watt <electricninja102@gmail.com>
2024-07-26 17:48:18 -0600

    Updated Outline

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -4,9 +4,13 @@ import { useNavigate } from "react-router";
 export default function UserName() {
 	// const navigate = useNavigate();
 
+	
+
 	return (
 		<div>
       <p>TODO: Create frontend. </p>
 		</div>
 	);
 }
+
+
