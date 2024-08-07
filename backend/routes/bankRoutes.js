5d4c27f2a233132d88a00919136eee0dd3065bad
Coleton Watt <electricninja102@gmail.com>
2024-08-07 13:12:38 -0600

    Separated a File

--- backend/routes/bankRoutes.js
+++ backend/routes/bankRoutes.js
@@ -76,61 +76,7 @@ bank.route("/createAccount").post(async (req, res) => {
 
 
 
-bank.route("/cu-deposit").post(async (req, res) =>{
-  try{
-    if(!req.session.username){
-      return res.status(301).json("Not Logged In")
-    }
-    const accountNa = req.body.accountName
-    const totalChange = req.body.depositAmount
 
-    const db_connect = dbo.getDb().collection("words");
-
-    const bankDetails = await db_connect.findOne(
-      {username: req.session.username, passHash: req.session.passHash}
-    )
-
-    const details = (bankDetails.account).map((account)=> {
-      if (account.accountName == accountNa){
-        account.amount += totalChange
-        account.history.push({
-          type: "Deposit",
-          amount: totalChange,
-          date: Date.now(),
-          recipient: account.accountName,
-        })
-      }
-    })
-    const x = await db_connect.findOneAndReplace({username: req.session.username, passHash: req.session.passHash}, details)
-
-	res.status(200).json({
-    x
-	})
-
-  }catch(err){
-    throw err;
-  }
-});
-
-bank.route("/cu-accountDetails").post(async (req, res) =>{
-  try{
-    if(!req.session.username){
-      return res.status(301).json("Not Logged In")
-    }
-    const accountNa = req.body.accountName
-    const totalChange = req.body.depositAmount
-
-    const db_connect = dbo.getDb().collection("words");
-
-    const bankDetails = await db_connect.findOne(
-      {username: req.session.username, passHash: req.session.passHash}
-    )
-
-    return res.status(200).json(bankDetails)
-  }catch(err){
-    throw err
-  }
-});
 
 
 
