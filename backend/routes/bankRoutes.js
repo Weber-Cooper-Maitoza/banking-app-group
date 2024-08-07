a05580ff2af9371aca3dc4e0c3827086ecce978e
BrodyGGG <Brodygardner@mail.weber.edu>
2024-08-06 22:11:51 -0600

    updates

--- backend/routes/bankRoutes.js
+++ backend/routes/bankRoutes.js
@@ -14,17 +14,17 @@ const hashPass = (password) => {
 // Create account route
 bank.post("/createAccount", async (req, res) => {
   const db_connect = dbo.getDb().collection("accounts");
-  const { userName, firstname, lastname, email, phone, password, role } = req.body;
+  const { username, firstname, lastname, email, phone, password, role } = req.body;
 
   try {
     console.log(req.body);
 
-    if (!userName || !firstname || !lastname || !email || !phone || !password || !role) {
+    if (!username || !firstname || !lastname || !email || !phone || !password || !role) {
       return res.status(400).json("All fields are required");
     }
 
     // Check if existing user
-    const exists = await db_connect.findOne({ username: userName });
+    const exists = await db_connect.findOne({ username: username });
     if (exists) {
       return res.status(400).json("Username already exists");
     }
@@ -42,7 +42,7 @@ bank.post("/createAccount", async (req, res) => {
       firstname: firstname,
       lastname: lastname,
       customerId: userId,
-      username: userName,
+      username: username,
       email: email,
       phone: phone,
       passHash: newPassword,
@@ -55,10 +55,10 @@ bank.post("/createAccount", async (req, res) => {
     };
 
     await db_connect.insertOne(newAccount);
-    res.status(201).json({ message: "Account created successfully!" });
+    res.status(201).json("Account created successfully!") ;
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Internal server error" });
+    res.status(500).json("Error inside create account" );
   }
   });
 
@@ -88,7 +88,7 @@ bank.post("/login", async (req, res) => {
       res.status(200).json("Login successful!" );
     } catch (error) {
       console.error(error);
-      res.status(400).json("Internal server error");
+      res.status(400).json("Error in Login");
     }
   });
   
@@ -96,10 +96,179 @@ bank.post("/login", async (req, res) => {
 bank.post("/logout", async (req, res) => {
     req.session.destroy((err) => {
         if (err) {
-        return res.status(301).json({ message: "Failed to log out" });
+        return res.status(301).json("Failed to log out");
         }
-        res.status(400).json("Logout successful");
+        res.status(200).json("Logout successful");
     });
 });
 
+
+
+
+
+///////delete all of this , this is from test routes 
+bank.route("/accountDetails").post(async (req, res) => {
+	// const details = (req.session.accounts).filter((account)=> (
+	// 	account.accountName == req.body.accountName
+	// ))
+
+	res.status(200).json({
+		username: req.session.username,
+		role: req.session.role,
+		accounts: req.session.accounts,
+		firstname: req.session.firstname,
+		lastname: req.session.lastname,
+	});
+});
+
+
+bank.route("/role").get(async (req, res) => {
+	res.status(200).json({
+		role: req.session.role
+	})
+})
+
+
+bank.route("/changeRole").post(async (req, res) => {
+	req.session.role = req.body.role
+
+	res.status(200).json({
+		role: req.session.role
+	})
+})
+
+// Coleton
+bank.route("/withdraw").post(async (req, res) => {
+	const accountNa = req.body.accountName
+	const totalChange = -req.body.withdrawAmount
+
+	const details = (req.session.accounts).map((account)=> {
+		if (account.accountName == accountNa){
+			account.amount += totalChange
+			account.history.push({
+				type: "Withdraw",
+				amount: totalChange,
+				date: Date.now(),
+				recipient: account.accountName,
+			})
+		}
+		return account
+	})
+	req.session.accounts
+
+	res.status(200).json({
+	})
+})
+
+//Coleton
+bank.route("/deposit").post(async (req, res) => {
+	const accountNa = req.body.accountName
+	const totalChange = req.body.depositAmount
+
+	const userAccount = req.session.accounts
+
+	const details = (userAccount).map((account)=> {
+		if (account.accountName == accountNa){
+			account.amount += totalChange
+			account.history.push({
+				type: "Deposit",
+				amount: totalChange,
+				date: Date.now(),
+				recipient: account.accountName,
+			})
+		}
+		return account
+	})
+
+	res.status(200).json({
+	})
+})
+
+// Brody
+bank.route("/createAccount").post(async (req, res) => {
+  try {
+    let db_connect = dbo.getDb();
+    let myobj = {
+		username: req.body.username,
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      email: req.body.email,
+      phone: req.body.phone,
+      password: req.body.password,
+      roles: "",
+      // savings: 0,
+      // checking: 0
+    };
+    const check = await db_connect.collection("accounts").findOne({email: req.body.email});
+    // if (check != null) {
+    //   res.json({check: false});
+    //   return;
+    // }
+    db_connect.collection("accounts").insertOne(myobj);
+    res.json({check: true});
+  } catch(err) {
+    throw err;
+  }
+});
+
+
+bank.route("/logout").get(async (req, res) => {
+	req.session.destroy();
+	let status = "No session set";
+	const resultObj = { status: status };
+	res.json(resultObj);
+});
+
+
+
+//Copper
+bank.route("/checkCustomerID/:id").post(async (req, res) => {
+  try {
+		if (customer.customerid == req.params.id) {
+			req.session.customerSearch = customer.customerid;
+			res.status(200).json({
+				check: true
+			});
+		} else {
+			req.session.customerSearch = "";
+			res.status(301).json({check: false})
+		}
+  } catch(err) {
+    throw err;
+  }
+});
+
+//
+bank.route("/getCustomerSummary").post(async (req, res) => {
+	try {
+		if (customer.customerid == req.session.customerSearch) {
+			res.status(200).json({
+				customerid: customer.customerid,
+				firstname: customer.firstname,
+				lastname: customer.lastname,
+				username: customer.username,
+				role: customer.role,
+				accounts: customer.accounts,
+				check: true
+			});
+		} else {
+			res.status(301).json ({
+				check: false
+			})
+		}
+	} catch(err) {
+		throw err;
+	}
+});
+
+//
+bank.route("/changeCustomerRole").post(async (req, res) => {
+	try {
+		customer.role = req.body.role
+
+		res.status(200).json({ role: customer.role});
+	} catch(err) {
+		throw err;
+	}
+})
 module.exports = bank
--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -11,26 +11,25 @@ export default function Login() {
 
     function updateForm(jsonObj) {
         return setForm((prevJsonObj) => {
-            return {...prevJsonObj, ...jsonObj}
+            return { ...prevJsonObj, ...jsonObj };
         });
     }
-    // useEffect(() => {
-    //     async function logout(){
-    //         console.log("logout");
-    //         const response = fetch(`http://localhost:5001/logout`, {
-    //           method: "GET",
-    //           credentials: "include",
-    //         });
-    //         console.log(response)
-    //     }
-    //     logout()
 
-    // },[])
+    useEffect(() => {
+        async function logout(){
+            console.log("logout");
+            const response = await fetch(`http://localhost:5001/logout`, {
+              method: "GET",
+              credentials: "include",
+            });
+            console.log(response)
+        }
+        logout()
+    }, [])
 
-    async function onSubmit(e){
-        
+    async function onSubmit(e) {
         e.preventDefault();
-        const info = {...form};
+        const info = { ...form };
         const attempt = await fetch("http://localhost:5001/login", {
             method: "POST",
             headers: {
@@ -38,50 +37,50 @@ export default function Login() {
             },
             body: JSON.stringify(info),
             credentials: "include"
-        })
-        if(!attempt.ok){
+        });
+
+        if (!attempt.ok) {
             window.alert("Incorrect user name or password");
             setForm({ userName: "", password: "" });
             console.log("Bad user name or password");
             return;
         }
-        setForm({ userName: "", Password: "" });
+        
+        setForm({ userName: "", password: "" });
         navigate("/c-account");
     }
 
-    
-
     return (
-        <div className= "container mt-4">
+        <div className="container mt-4">
             <h3 className="pt-3">Account Login</h3>
             <form onSubmit={onSubmit}>
                 <div className="col-3">
                     <label>User Name:</label>
                     <input 
-                        type = "text"
-                        id = "userName"
+                        type="text"
+                        id="userName"
                         value={form.userName}
-                        onChange={(e) => updateForm({userName: e.target.value })}
-						className="form-control"    
+                        onChange={(e) => updateForm({ userName: e.target.value })}
+                        className="form-control"    
                     />
                 </div>
                 <div className="col-3">
                     <label>Password:</label>
                     <input 
-                        type = "password"
-                        id = "password"
+                        type="password"
+                        id="password"
                         value={form.password}
-                        onChange={(e) => updateForm({password: e.target.value })} 
-						className="form-control"  
+                        onChange={(e) => updateForm({ password: e.target.value })}
+                        className="form-control"  
                     />
                 </div>
                 <br/>
                 <div className="mx-5">
                     <input 
-						type="submit" 
-						value="Login"
-						className="btn btn-secondary my-2"
-					/>
+                        type="submit" 
+                        value="Login"
+                        className="btn btn-secondary my-2"
+                    />
                 </div>
             </form>
         </div>
